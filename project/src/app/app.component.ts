698725c12617d0e48299335bc02af9070e2d99d2
Daniel Lim <hbt@markhamboards.com>
2016-12-20 17:12:00 -0500

    update Extend Feature
    update Extend Feature

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -12,6 +12,8 @@ import { Category } from './Category';
 export class AppComponent implements OnInit {
   courses:Course[];
   categories:Category[];
+  appState: string;
+  activeKey: string;
 
   constructor(private _firebaseService: FirebaseService) {
   }
@@ -29,4 +31,15 @@ export class AppComponent implements OnInit {
             this.categories = categories;
         });
   }
+
+
+  changeState(state, key){
+    console.log('Changing state to: '+state);
+    if(key){
+      console.log('Changing key to: '+key);
+      this.activeKey = key;
+    }
+    this.appState = state;
+  }
+
 }
