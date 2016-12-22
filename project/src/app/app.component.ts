ea5a43026ea43fc606ab0bde60eb059529dc53bc
Daniel Lim <hbt@markhamboards.com>
2016-12-22 12:36:51 -0500

    add Review Component
    add Review Component

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from './services/firebase.service';
+// import { HTTP_PROVIDERS } from '@angular/http';
 import { Course } from './Course';
 import { Category } from './Category';
 import { Status } from './Status';
-import { Reviews } from './Reviews';
+// import { Reviews } from './Reviews';
+import { ReviewComponent } from './components/review.component';
+// import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],  
+  // directives:[ReviewComponent],
   providers: [FirebaseService]
 })
 
---
+++ project/src/app/components/review.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Component({
+    selector: 'review',
+    templateUrl: './app/components/review.component.html',
+    styleUrls: ['./app/components/review.component.css']
+})
+
+export class ReviewComponent {
+  
+}
