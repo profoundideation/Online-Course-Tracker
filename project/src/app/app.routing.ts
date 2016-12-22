a3e750d3b0984d1d8d4aa7dc3ffcc6ab7656fad4
Daniel Lim <hbt@markhamboards.com>
2016-12-22 14:35:14 -0500

    Home and Profile
    Home and Profile

--- project/src/app/app.routing.ts
+++ project/src/app/app.routing.ts
@@ -1,2 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
---
+++ project/src/app/components/profile/profile.component.html
@@ -0,0 +1,1 @@
+<h3>User Profile</h3>
