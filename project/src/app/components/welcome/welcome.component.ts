53f4afe6686198eba8af8ab81360fd39e89a2309
Daniel Lim <hbt@markhamboards.com>
2016-12-25 19:30:04 -0500

    add Welcome Comp.

--- project/src/app/components/welcome/welcome.component.ts
+++ project/src/app/components/welcome/welcome.component.ts
@@ -3,10 +3,14 @@ import { Auth } from '../../services/auth.service';
 
 @Component({
   selector: 'welcome',
-  templateUrl: 'welcome.component.html'
+  templateUrl: 'welcome.component.html',  
+  styleUrls: [ 'welcome.component.css' ]
 })
 
 export class WelcomeComponent { 
+
   constructor(private auth: Auth) {
+      
   }
+    
 }
