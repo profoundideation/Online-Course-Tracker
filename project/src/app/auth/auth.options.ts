59d5e1963160ddbf25b48fc6bbb42ee3c6fec8d0
Daniel Lim <hbt@markhamboards.com>
2016-12-29 10:16:42 -0500

    update Auth
    update Auth

--- project/src/app/auth/auth.options.ts
+++ project/src/app/auth/auth.options.ts
@@ -1,16 +1,16 @@
 export const options = {
-    allowedConnections: ['twitter', 'facebook', 'linkedin', 'github', 'Username-Password-Authentication'],    
-    theme: {
-        logo: 'http://memberclouds.com/members/wp-content/uploads/2015/08/MC-logo07-copy.png',
-    },
-    auth: {
-        sso: true
-    },
-    additionalSignUpFields: [{
-        name: "github_handle",
-        placeholder: "Enter your GitHub Handle"
-      }],
-      languageDictionary: {        
-        title: "Online Course Tracker"
-      }  
+     allowedConnections: ['twitter', 'facebook', 'linkedin', 'github', 'Username-Password-Authentication'],
+     theme: {
+          logo: 'http://memberclouds.com/members/wp-content/uploads/2015/08/MC-logo07-copy.png',
+     },
+     auth: {
+          sso: true
+     },
+     additionalSignUpFields: [{
+          name: "github_handle",
+          placeholder: "Enter your GitHub Handle"
+     }],
+     languageDictionary: {
+          title: "Online Course Tracker"
+     }
 }
