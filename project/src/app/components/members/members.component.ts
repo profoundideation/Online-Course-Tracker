20e0c7d8cf6f6092bcf6b26c7ea9dd8447e51ed9
Daniel Lim <hbt@markhamboards.com>
2016-12-30 12:23:53 -0500

    Keys

--- project/src/app/components/members/members.component.ts
+++ project/src/app/components/members/members.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Auth } from '../../services/auth.service';
 
+declare var i: any;
+
 @Component({
   selector: 'members',
   styleUrls: [ 'members.component.css' ],
@@ -9,6 +11,28 @@ import { Auth } from '../../services/auth.service';
 
 export class MembersComponent {
   constructor(private auth: Auth) {
+     console.log("local storage");
+    for (let i = 0; i < localStorage.length; i++)   {
+        console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
+    }
+    
+    console.log("session storage");
+    for (let i = 0; i < sessionStorage.length; i++) {
+        console.log(sessionStorage.key(i) + "=[" + sessionStorage.getItem(sessionStorage.key(i)) + "]");
+    }
   }
+  /*
+  display() {
+    console.log("local storage");
+    for (i = 0; i < localStorage.length; i++)   {
+        console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
+    }
+    
+    console.log("session storage");
+    for (i = 0; i < sessionStorage.length; i++) {
+        console.log(sessionStorage.key(i) + "=[" + sessionStorage.getItem(sessionStorage.key(i)) + "]");
+    }
+  }
+  */
 
 }
