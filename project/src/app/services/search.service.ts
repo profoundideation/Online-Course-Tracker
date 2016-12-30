55f898267f37ab13dbe0edafa6c3544b14aae9fc
Daniel Lim <hbt@markhamboards.com>
2016-12-30 10:52:03 -0500

    + Search

--- project/src/app/services/search.service.ts
+++ project/src/app/services/search.service.ts
@@ -1,10 +1,11 @@
 
-var fb = firebase.database.ref();
+// var fb = firebase.database.ref();
 
 /**
  * @param {string} emailAddress
  * @return {Object} the object contains zero or more user records, the keys are the users' ids
  */
+
 function findUsersMatchingEmail( emailAddress, callback ) {
     fb.child('user').orderByChild('emailAddress').equalTo(emailAddress).once('value', function(snap) {
         callback( snap.val() );
