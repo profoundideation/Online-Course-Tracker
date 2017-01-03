2b28abb2911b44c24a4db6815b7556a32562ece6
Daniel Lim <hbt@markhamboards.com>
2017-01-03 13:00:05 -0500

    re install npm depends

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { 
+  AngularFireModule, 
+  AuthMethods, 
+  AuthProviders 
+} from "angularfire2";
 
 // import { } angular-ui
 // import { } angular-material
@@ -10,6 +15,7 @@ import { AUTH_PROVIDERS } from 'angular2-jwt';
 // import { } auth0-angular
 
 // MATERIAL DESIGN MODULES
+/*
 import { MdToolbarModule } from '@angular2-material/toolbar';
 import { MdButtonModule } from '@angular2-material/button';
 import { MdCardModule } from '@angular2-material/card';
@@ -19,6 +25,7 @@ export let MD_MODULES: any = [
   MdButtonModule,
   MdCardModule
 ];
+*/
 
 import { routing, appRoutingProviders } from './app.routing';
 import { AppComponent } from './app.component';
@@ -34,14 +41,11 @@ import { TestingComponent } from './components/testing/testing.component';
 import { MembersComponent } from './components/members/members.component';
 import { LoginComponent } from './components/login/login.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
-import { 
-  AngularFireModule, 
-  AuthMethods, 
-  AuthProviders 
-} from "angularfire2";
 
 /* Feature Modules */
+/*
 import { CoreModule }       from './core.module';
+*/
 
 // import { EditformComponent } from './components/editform/editform.component';
 // import { ExtendsectionComponent } from './components/extendsection/extendsection.component';
